c60df5178345c7d5cb689e88d8931265f5830b43
Vyacheslav Stetskevych <slava18@gmail.com>
2012-12-23 15:53:57 +0200

    Collecting more stuff

---
+++ mountdisks.bat
@@ -0,0 +1,13 @@
+@echo off
+mountvol E:\ \\?\Volume{061ef765-884f-11e1-be7d-50e5495a280f}
+mountvol F:\ \\?\Volume{061ef766-884f-11e1-be7d-50e5495a280f}
+mountvol G:\ \\?\Volume{061ef767-884f-11e1-be7d-50e5495a280f}
+mountvol H:\ \\?\Volume{f80d140e-8c5c-11e1-bbc3-806e6f6e6963}
+
+timeout /t 2
+
+net share documents=E: /GRANT:Guest,CHANGE
+net share photo=F: /GRANT:Guest,CHANGE
+net share video=G: /GRANT:Guest,CHANGE
+net share music=H: /GRANT:Guest,CHANGE
+@echo on
--- ssh-agent-startup.sh
+++ ssh-agent-startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ## This goes in /etc/kde/env/ssh-agent-startup.sh and ~/.bashrc
-## Run ssh-agent only if not already running, and available
+## Run ssh-agent in only ONE copy per user
 pgrep -u "$(whoami)" ssh-agent &>/dev/null || ssh-agent -s > ~/.ssh_agent_env
 source ~/.ssh_agent_env >/dev/null
 
