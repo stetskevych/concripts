84b5bf8180c14bc46de608de47703596fd7871d7
Vyacheslav Stetskevych <vyacheslav.stetskevych@archer-soft.com>
2013-04-18 14:18:43 +0300

    Added a remark to fs creation

--- mountdisks.bat
+++ mountdisks.bat
@@ -1,3 +1,4 @@
+REM Preparing disk for ext2ifs: mke2fs -I 128 -m0 -L VIDEO -t ext3 -T largefile /dev/sda7
 @echo off
 mountvol E:\ \\?\Volume{061ef765-884f-11e1-be7d-50e5495a280f}
 mountvol F:\ \\?\Volume{061ef766-884f-11e1-be7d-50e5495a280f}
