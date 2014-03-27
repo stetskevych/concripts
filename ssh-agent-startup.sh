a3502db2ce8eaa14a46775f0e132569cac955b6f
Vyacheslav Stetskevych <slava18@gmail.com>
2014-03-27 11:53:55 +0200

    Add fix for ssh forwarding and gnu screen on the server

--- ssh-agent-startup.sh
+++ ssh-agent-startup.sh
@@ -4,3 +4,12 @@
 pgrep -u "$(whoami)" ssh-agent &>/dev/null || ssh-agent -s > ~/.ssh_agent_env
 source ~/.ssh_agent_env >/dev/null
 
+# fix ssh agent forwarding on jumpin
+if [ "$TERM" = "screen" ]; then
+  . ~/.sshvars
+else # plain ssh login
+  for SSHVAR in SSH_CLIENT SSH_TTY SSH_AUTH_SOCK SSH_CONNECTION DISPLAY; do
+    echo "export ${SSHVAR}=\"${!SSHVAR}\""
+  done > ~/.sshvars
+fi
+
