acf6c77cf1dbde6859230aaa3240ff980ea05ce7
Vyacheslav Stetskevych <slava18@gmail.com>
2016-11-24 12:41:52 +0200

    Add xz as an optional compressor, introduce pipefail

--- vst_full_backup_local.sh
+++ vst_full_backup_local.sh
@@ -11,8 +11,12 @@ fi
 USER='vst'
 DATE="$(date +%Y%m%d)"
 DIR="/home/$USER/backup/$DATE"
+COMPRESSOR='pigz -9'
+COMPRESSEXT='gz'
+#COMPRESSOR='xz -c -z - --threads=0'
+#COMPRESSEXT='xz'
 
-dotfiles=(.bash_history .bash_logout .bash_profile .bashrc .screenrc .config .fontconfig .fonts .gnupg .kde .kde4 .ssh .aws .Skype)
+dotfiles=(.bash_history .bash_logout .bash_profile .bashrc .screenrc .config .kde4 .ssh .aws .Skype)
 
 [[ ! -d "$DIR" ]] && mkdir -p $DIR
 
@@ -28,18 +32,22 @@ read -s -p "GPG Passphrase: " PASS
 yes | pacman -Sc
 journalctl --vacuum-size=100M
 
-[[ ! -p backup ]] && mkfifo backup
-exec 3<>backup
+[[ ! -p backup.fifo ]] && mkfifo backup.fifo
+exec 3<>backup.fifo
+
+set -euo pipefail
 
 echo "$PASS" >&3
-tar -cvf - -C /boot --numeric-owner --preserve-permissions --one-file-system /boot | pigz -9 | gpg -c --batch --no-tty --yes --passphrase-fd 3 -o $DIR/arch-boot-${DATE}.tar.gz.gpg
+tar -cvf - -C /boot --numeric-owner --preserve-permissions --one-file-system /boot | $COMPRESSOR | gpg -c --batch --no-tty --yes --passphrase-fd 3 -o $DIR/arch-boot-${DATE}.tar.$COMPRESSEXT.gpg
 echo "$PASS" >&3
-tar -cvf - -C /home/"$USER" --numeric-owner --preserve-permissions --one-file-system "${dotfiles[@]}" | pigz -9 | gpg -c --batch --no-tty --yes --passphrase-fd 3 -o $DIR/arch-dotfiles-${DATE}.tar.gz.gpg
+tar -cvf - -C /home/"$USER" --numeric-owner --preserve-permissions --one-file-system "${dotfiles[@]}" | $COMPRESSOR | gpg -c --batch --no-tty --yes --passphrase-fd 3 -o $DIR/arch-dotfiles-${DATE}.tar.$COMPRESSEXT.gpg
 echo "$PASS" >&3
-tar -cvf - -C / --numeric-owner --preserve-permissions --one-file-system / | pigz -9 | gpg -c --batch --no-tty --yes --passphrase-fd 3 -o $DIR/arch-root-${DATE}.tar.gz.gpg
+tar -cvf - -C / --numeric-owner --preserve-permissions --one-file-system / | $COMPRESSOR | gpg -c --batch --no-tty --yes --passphrase-fd 3 -o $DIR/arch-root-${DATE}.tar.$COMPRESSEXT.gpg
+
+unset PASS
 
 chown -R "$USER":users "$DIR"
 
-unset PASS
+rm backup.fifo
 
 echo "done"
